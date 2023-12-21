2b2c791e3ef021406b631e566f0153db59d04cf9
Justus Güttler <jguettler@gunter.software>
2023-12-21 18:03:39 +0100

    Cleaned up the code with a generic class.

--- src/main/java/software/gunter/naturesniche/NaturesNicheConfig.java
+++ src/main/java/software/gunter/naturesniche/NaturesNicheConfig.java
@@ -1,19 +1,34 @@
 package software.gunter.naturesniche;
 
+import net.minecraft.block.Block;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
 import software.gunter.naturesniche.interfaces.IConfig;
 import software.gunter.naturesniche.utils.RegistryUtil;
 
 import java.util.*;
+import java.util.function.Function;
 
 public class NaturesNicheConfig {
+    private final Map<String, ModifierConfig> crops = new HashMap<>();
+    private final Map<String, ModifierConfig> biomes = new HashMap<>();
+    private final String configOrder = "cropFirst"; // Default is "cropFirst". Possible["cropFirst", "biomeFirst"]
+
     public void updateCrop(String identifier) {
         NaturesNicheMod.LOGGER.info("Updating crop " + identifier + "...");
-        CropConfig cropConfig = (CropConfig) crops.getOrDefault(identifier, new CropConfig());
-        cropConfig.update();
+        ModifierConfig cropConfig = crops.getOrDefault(identifier, new ModifierConfig());
+        cropConfig.updateModifiers(RegistryUtil.getBiomes(), biome -> String.valueOf(BuiltinRegistries.BIOME.getId((Biome) biome)));
         crops.put(identifier, cropConfig);
         NaturesNicheMod.LOGGER.info("Crop " + identifier + " updated.");
+    }
+
+    public void updateBiome(String identifier) {
+        NaturesNicheMod.LOGGER.info("Updating biome " + identifier + "...");
+        ModifierConfig biomeConfig = biomes.getOrDefault(identifier, new ModifierConfig());
+        biomeConfig.updateModifiers(RegistryUtil.getCrops(), crop -> String.valueOf(Registry.BLOCK.getId((Block) crop)));
+        biomes.put(identifier, biomeConfig);
+        NaturesNicheMod.LOGGER.info("Biome " + identifier + " updated.");
     }
 
     public void updateCrops() {
@@ -25,86 +40,12 @@ public class NaturesNicheConfig {
         });
     }
 
-    public void updateBiome(String identifier) {
-        NaturesNicheMod.LOGGER.info("Updating biome " + identifier + "...");
-        BiomeConfig biomeConfig = (BiomeConfig) biomes.getOrDefault(identifier, new BiomeConfig());
-        biomeConfig.update();
-        biomes.put(identifier, biomeConfig);
-        NaturesNicheMod.LOGGER.info("Biome " + identifier + " updated.");
-    }
-
     public void updateBiomes() {
         RegistryUtil.getBiomes().forEach(biome -> {
             String biomeIdentifier = String.valueOf(BuiltinRegistries.BIOME.getId(biome));
             updateBiome(biomeIdentifier);
         });
     }
-    private final Map<String, IConfig> crops = new HashMap<>();
-    private final Map<String, IConfig> biomes = new HashMap<>();
-
-    private static class CropConfig implements IConfig {
-        private final Map<String, Float> biomeModifier;
-
-        public CropConfig(Map<String, Float> biomeModifier) {
-            this.biomeModifier = biomeModifier;
-        }
-
-        public CropConfig() {
-            this(new HashMap<>());
-            update();
-        }
-
-        @Override
-        public void update() {
-            RegistryUtil.getBiomes().forEach(biome -> biomeModifier.putIfAbsent(String.valueOf(BuiltinRegistries.BIOME.getId(biome)), 1.0f));
-        }
-
-        @Override
-        public float getModifier(String biomeIdentifier) {
-            if (this.biomeModifier.containsKey(biomeIdentifier)) {
-                return this.biomeModifier.get(biomeIdentifier);
-            }
-            return 1.0f;
-        }
-
-        @Override
-        public Map<String, Float> getModifierMap() {
-            return biomeModifier;
-        }
-    }
-
-    private static class BiomeConfig implements IConfig {
-        private final Map<String, Float> cropModifier;
-
-        public BiomeConfig(Map<String, Float> cropModifier) {
-            this.cropModifier = cropModifier;
-        }
-
-        public BiomeConfig() {
-            this(new HashMap<>());
-            update();
-        }
-
-        @Override
-        public void update() {
-            RegistryUtil.getCrops().forEach(crop -> cropModifier.putIfAbsent(String.valueOf(Registry.BLOCK.getId(crop)), 1.0f));
-        }
-
-        @Override
-        public float getModifier(String cropIdentifier) {
-            if (this.cropModifier.containsKey(cropIdentifier)) {
-                return this.cropModifier.get(cropIdentifier);
-            }
-            return 1.0f;
-        }
-
-        @Override
-        public Map<String, Float> getModifierMap() {
-            return cropModifier;
-        }
-    }
-
-    private final String configOrder = "cropFirst"; // Default is "cropFirst". Possible["cropFirst", "biomeFirst"]
 
     public float getModifier(String cropIdentifier, String biomeIdentifier) {
         return getModifierBasedOnOrder(cropIdentifier, biomeIdentifier, "cropFirst".equals(configOrder));
@@ -118,8 +59,8 @@ public class NaturesNicheConfig {
         return checkConfig(cropFirst ? biomes : crops, cropFirst ? biomeIdentifier : cropIdentifier, cropFirst ? cropIdentifier : biomeIdentifier);
     }
 
-    private float checkConfig(Map<String, IConfig> configs, String primaryIdentifier, String secondaryIdentifier) {
-        IConfig config = configs.get(primaryIdentifier);
+    private float checkConfig(Map<String, ModifierConfig> configs, String primaryIdentifier, String secondaryIdentifier) {
+        ModifierConfig config = configs.get(primaryIdentifier);
         if (config != null) {
             return config.getModifier(secondaryIdentifier);
         }
@@ -133,5 +74,28 @@ public class NaturesNicheConfig {
 
     public String getConfigOrder() {
         return configOrder;
+    }
+
+    private static class ModifierConfig implements IConfig {
+        private final Map<String, Float> modifiers = new HashMap<>();
+
+        public void updateModifiers(Collection<?> elements, Function<Object, String> identifierFunction) {
+            elements.forEach(element -> modifiers.putIfAbsent(identifierFunction.apply(element), 1.0f));
+        }
+
+        @Override
+        public void update() {
+
+        }
+
+        @Override
+        public float getModifier(String identifier) {
+            return modifiers.getOrDefault(identifier, 1.0f);
+        }
+
+        @Override
+        public Map<String, Float> getModifierMap() {
+            return modifiers;
+        }
     }
 }
