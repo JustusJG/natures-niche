7668e21e98d26a466bd75b635c0654294ce0e459
Justus Güttler <jguettler@gunter.software>
2023-12-21 17:05:53 +0100

    Optimized code by removing redundancy.

--- src/main/java/software/gunter/naturesniche/commands/NaturesNicheCommand.java
+++ src/main/java/software/gunter/naturesniche/commands/NaturesNicheCommand.java
@@ -1,11 +1,14 @@
 package software.gunter.naturesniche.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.command.argument.IdentifierArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.LiteralText;
 import software.gunter.naturesniche.NaturesNicheMod;
+
+import javax.annotation.Nullable;
 
 public class NaturesNicheCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
@@ -18,44 +21,46 @@ public class NaturesNicheCommand {
                                     return 1;
                                 })
                         )
-                        .then(CommandManager.literal("crop")
-                                .then(CommandManager.literal("update")
-                                        .executes(context -> {
-                                            NaturesNicheMod.CONFIG.updateCrops();
-                                            NaturesNicheMod.CONFIG_MANAGER.saveConfig();
-                                            context.getSource().sendFeedback(new LiteralText("Fehlende Konfiguationen den Crops hinzugefügt."), true);
-                                            return 1;
-                                        })
-                                        .then(CommandManager.argument("identifier", IdentifierArgumentType.identifier())
-                                                .executes(context -> {
-                                                    String identifier = IdentifierArgumentType.getIdentifier(context, "identifier").toString();
-                                                    NaturesNicheMod.CONFIG.updateCrop(identifier);
-                                                    NaturesNicheMod.CONFIG_MANAGER.saveConfig();
-                                                    context.getSource().sendFeedback(new LiteralText("Fehlende Konfigurationen dem Crop " + identifier + " hinzugefügt."), true);
-                                                    return 1;
-                                                })
-                                        )
-                                )
-                        )
-
-                        .then(CommandManager.literal("biome")
-                                .executes(context -> {
-                                    NaturesNicheMod.CONFIG.updateBiomes();
-                                    NaturesNicheMod.CONFIG_MANAGER.saveConfig();
-                                    context.getSource().sendFeedback(new LiteralText("Fehlende Konfiguationen den Biomen hinzugefügt."), true);
-                                    return 1;
-                                })
+                        .then(CommandManager.literal("crop").executes(context -> updateConfig(context, "crop", null))
                                 .then(CommandManager.argument("identifier", IdentifierArgumentType.identifier())
                                         .executes(context -> {
                                             String identifier = IdentifierArgumentType.getIdentifier(context, "identifier").toString();
-                                            NaturesNicheMod.CONFIG.updateBiome(identifier);
-                                            NaturesNicheMod.CONFIG_MANAGER.saveConfig();
-                                            context.getSource().sendFeedback(new LiteralText("Fehlende Konfigurationen dem Biome " + identifier + " hinzugefügt."), true);
-                                            return 1;
+                                            return updateConfig(context, "crop", identifier);
+                                        })
+                                )
+                        )
+
+                        .then(CommandManager.literal("biome").executes(context -> updateConfig(context, "biome", null))
+                                .then(CommandManager.argument("identifier", IdentifierArgumentType.identifier())
+                                        .executes(context -> {
+                                            String identifier = IdentifierArgumentType.getIdentifier(context, "identifier").toString();
+                                            return updateConfig(context, "biome", identifier);
                                         })
                                 )
                         )
                 )
         );
+    }
+
+    private static int updateConfig(CommandContext<ServerCommandSource> context, String type, @Nullable String identifier) {
+        if (type.equals("crop")) {
+            if (identifier == null) {
+                NaturesNicheMod.CONFIG.updateCrops();
+            } else {
+                NaturesNicheMod.CONFIG.updateCrop(identifier);
+            }
+        } else if (type.equals("biome")) {
+            if (identifier == null) {
+                NaturesNicheMod.CONFIG.updateBiomes();
+            } else {
+                NaturesNicheMod.CONFIG.updateBiome(identifier);
+            }
+        }
+
+        NaturesNicheMod.CONFIG_MANAGER.saveConfig();
+        String feedbackMessage = "Fehlende Konfigurationen " + (identifier == null ? "hinzugefügt." : "dem " + type + " " + identifier + " hinzugefügt.");
+        context.getSource().sendFeedback(new LiteralText(feedbackMessage), true);
+
+        return 1;
     }
 }
