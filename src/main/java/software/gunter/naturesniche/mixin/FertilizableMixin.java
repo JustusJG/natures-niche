bfca0f4b6d9eae80a6dd520a18cbf12db82a118c
Justus Güttler <jguettler@gunter.software>
2023-12-21 05:30:29 +0100

    I don't know. IDE say red but it looks OK.

--- src/main/java/software/gunter/naturesniche/mixin/FertilizableMixin.java
+++ src/main/java/software/gunter/naturesniche/mixin/FertilizableMixin.java
@@ -2,16 +2,24 @@ package software.gunter.naturesniche.mixin;
 
 import net.minecraft.block.*;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import software.gunter.naturesniche.NaturesNicheMod;
 
+import java.util.Optional;
 import java.util.Random;
 
 @Mixin({CropBlock.class, CocoaBlock.class, StemBlock.class, SaplingBlock.class})
 public abstract class FertilizableMixin extends Block implements Fertilizable {
+    @Shadow public abstract void grow(ServerWorld world, Random random, BlockPos pos, BlockState state);
 
     public FertilizableMixin(Settings settings) {
         super(settings);
@@ -19,11 +27,43 @@ public abstract class FertilizableMixin extends Block implements Fertilizable {
 
     @Inject(at = @At("HEAD"), method = "randomTick", cancellable = true)
     public void randomTickInject(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
-        ci.cancel();
+        Identifier cropIdentifier = Registry.BLOCK.getId(state.getBlock());
+        Optional<RegistryKey<Biome>> biomeKeyOptional = world.getBiome(pos).getKey();
+
+        if (biomeKeyOptional.isPresent()) {
+            Identifier biomeIdentifier = biomeKeyOptional.getValue();
+            float multiplier = NaturesNicheMod.CONFIG.getModifier(cropIdentifier, biomeIdentifier);
+            if (multiplier < 1.0f) {
+                while (multiplier > 0f) {
+                    float rand = random.nextFloat();
+                    if (multiplier >= rand) {
+                        this.randomTick(state, world, pos, random);
+                        multiplier -= 1f;
+                    }
+                }
+                ci.cancel();
+            }
+        }
     }
 
     @Inject(at = @At("HEAD"), method = "grow", cancellable = true)
     public void growInject(ServerWorld world, Random random, BlockPos pos, BlockState state, CallbackInfo ci) {
-        ci.cancel();
+        Identifier cropIdentifier = Registry.BLOCK.getId(state.getBlock());
+        Optional<RegistryKey<Biome>> biomeKeyOptional = world.getBiome(pos).getKey();
+
+        if (biomeKeyOptional.isPresent()) {
+            Identifier biomeIdentifier = biomeKeyOptional.getValue();
+            float multiplier = NaturesNicheMod.CONFIG.getModifier(cropIdentifier, biomeIdentifier);
+            if (multiplier < 1.0f) {
+                while (multiplier > 0f) {
+                    float rand = random.nextFloat();
+                    if (multiplier >= rand) {
+                        this.grow(world, random, pos, state);
+                        multiplier -= 1f;
+                    }
+                }
+                ci.cancel();
+            }
+        }
     }
 }
