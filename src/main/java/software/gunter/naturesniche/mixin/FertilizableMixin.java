aeab96d171a9db4bef96d65f4a4c9ced266f28b8
Justus Güttler <jguettler@gunter.software>
2023-12-21 03:59:09 +0100

    Added almost empty Mixin class that will hopefully stop all plants from growing.

---
+++ src/main/java/software/gunter/naturesniche/mixin/FertilizableMixin.java
@@ -0,0 +1,29 @@
+package software.gunter.naturesniche.mixin;
+
+import net.minecraft.block.*;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
+
+@Mixin({CropBlock.class, CocoaBlock.class, StemBlock.class, SaplingBlock.class})
+public abstract class FertilizableMixin extends Block implements Fertilizable {
+
+    public FertilizableMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Inject(at = @At("HEAD"), method = "randomTick", cancellable = true)
+    public void randomTickInject(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        ci.cancel();
+    }
+
+    @Inject(at = @At("HEAD"), method = "grow", cancellable = true)
+    public void growInject(ServerWorld world, Random random, BlockPos pos, BlockState state, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
