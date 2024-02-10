4d20a3613c317c6e8aa9bac41f01fecfa57f4a55
Justus Güttler <jguettler@gunter.software>
2024-02-10 11:05:55 +0100

    Removed temperature cache

--- src/main/java/software/gunter/naturesniche/NaturesNicheMod.java
+++ src/main/java/software/gunter/naturesniche/NaturesNicheMod.java
@@ -20,33 +20,16 @@ import software.gunter.naturesniche.utils.ConfigManager;
 public class NaturesNicheMod implements ModInitializer {
     public static String MOD_ID = "natures-niche";
     public static final Logger LOGGER = LoggerFactory.getLogger("Natures Niche");
-
-    public static final ThreadLocal<Long2FloatLinkedOpenHashMap> TEMPERATURE_CACHE = ThreadLocal.withInitial(() -> Util.make(() -> {
-        Long2FloatLinkedOpenHashMap long2FloatLinkedOpenHashMap = new Long2FloatLinkedOpenHashMap(1024, 0.25f){
-
-            @Override
-            protected void rehash(int i) {
-            }
-        };
-        long2FloatLinkedOpenHashMap.defaultReturnValue(Float.NaN);
-        return long2FloatLinkedOpenHashMap;
-    }));
     public static ConfigManager CONFIG_MANAGER;
     public static NaturesNicheConfig CONFIG;
 
     @Override
     public void onInitialize() {
         License.iConfirmNonCommercialUse("gunter.software");
-        LOGGER.info("Hello Fabric world!");
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> NaturesNicheCommand.register(dispatcher));
 
         CONFIG = new NaturesNicheConfig();
         CONFIG_MANAGER = new ConfigManager();
-
-        ServerWorldEvents.LOAD.register((server, world) -> {
-            System.out.println(server);
-            System.out.println(world);
-        });
 
         ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
             @Override
@@ -57,7 +40,6 @@ public class NaturesNicheMod implements ModInitializer {
             @Override
             public void reload(ResourceManager manager) {
                 CONFIG_MANAGER.loadConfig(manager);
-                TEMPERATURE_CACHE.get().clear();
             }
         });
     }
